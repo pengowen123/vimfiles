642e64c1d9e1e75b1c0106f9d47a6bc0bc96e164
owen <pengowen123@gmail.com>
2016-04-11 01:36:58 -0700

    fix plugins.sh

--- plugins.sh
+++ plugins.sh
@@ -1,5 +1,6 @@
-# requirements vim, git, python, racer (added to path), and
-# visual studio community edition from:
+# requirements vim, pathogen, git, cmake, python,
+# racer (added to path), and visual studio
+# community edition from:
 # https://www.visualstudio.com/products/free-developer-offers-vs.aspx
 
 cd vimfiles/bundle
@@ -7,8 +8,9 @@ git clone https://github.com/scrooloose/nerdcommenter.git
 git clone https://github.com/scrooloose/nerdtree.git
 git clone https://github.com/scrooloose/syntastic.git
 git clone git://github.com/jeffkreeftmeijer/vim-numbertoggle.git
+git clone https://github.com/romainl/Apprentice.git
 git clone https://github.com/Valloric/YouCompleteMe.git
-git clone git://github.com/tommcdo/vim-exchange.git && python YouCompleteMe/install.py
+git clone git://github.com/tommcdo/vim-exchange.git && cd YouCompleteMe && git submodule update --init --recursive && python install.py --racer-completer
 
 # languages:
 git clone --depth=1 https://github.com/rust-lang/rust.vim.git
